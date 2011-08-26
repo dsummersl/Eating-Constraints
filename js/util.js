20ec9b16f89e4a5f464132dd776c09e2e4e46b3c
Dane Summers <dsummersl@yahoo.com>
2011-08-26 01:24:55 -0400

    added test cases and I have a basic rough working framework. much. to. do.

--- js/util.js
+++ js/util.js
@@ -1,4 +1,35 @@
 
+function drawLitmus(eater,element) {
+	// TODO generate the data points and widths
+	// draw a line underneath...so the entire area of the box equals the total serving size of the container
+}
+
+function combineIngredientsAndRanks(food) {
+	food.uniqueRankCategories = $.map(food.ingredientRanks, function (v) { return v.IngredientCategory }).filter(function(v) { return v != ''}).unique().sort()
+	// make all the food ingredients be an array, and each element of the array is a dictionary of the food details.
+	var newfood = []
+	for (k in food.ingredients) {
+		newfood.push(food.ingredients[k])
+	}
+	food.ingredients = newfood;
+	// same with the food detials, make it an array
+	newfood = [];
+	for (k in food.ingredientRanks) {
+		newfood.push(food.ingredientRanks[k]);
+	}
+	food.ingredientRanks = newfood;
+	$.each(food.ingredients,function(i,k) {
+		//console.log("look in key = "+ k.Description);
+		var matches = food.ingredientRanks.filter(function(v) { return v.Description == k.Description });
+		var justRanks = $.map(matches, function(v) { return v.IngredientCategory }).filter(function(v) { return v != ''}).unique().sort()
+		//console.log($.sprintf("%30s = %s",k.Description,JSON.stringify(matches)));
+		k.Ranks = matches;
+		k.Categories = justRanks;
+	});
+	delete food.ingredientRanks;
+	return food;
+}
+
 /*
  * Checks an array of data for existance called via function. when they all exist the
  * onExists function is called.
@@ -21,4 +52,136 @@ function checkData(dataFunc,interval,onExists) {
 		//console.log("calling exists, got all data")
 		onExists()
 	}
+}
+
+// obtain a unique list of elements in an array (depends on jquery)
+Array.prototype.unique = function() {
+	var vals = this;
+	var unique = [];
+	for (var i=vals.length;i--;) {
+		var val = vals[i];
+		if($.inArray(val,unique) == -1) {
+			unique.unshift(val);
+		}
+	}
+	return unique;
+}
+
+// From: http://stackoverflow.com/questions/1885557/simplest-code-for-array-intersection-in-javascript
+/* finds the intersection of 
+ * two arrays in a simple fashion.  
+ *
+ * PARAMS
+ *  a - first array, must already be sorted
+ *  b - second array, must already be sorted
+ *
+ * NOTES
+ *
+ *  Should have O(n) operations, where n is 
+ *    n = MIN(a.length(), b.length())
+ */
+function intersect_safe(a, b)
+{
+  var ai=0, bi=0;
+  var result = new Array();
+
+  while( ai < a.length && bi < b.length )
+  {
+     if      (a[ai] < b[bi] ){ ai++; }
+     else if (a[ai] > b[bi] ){ bi++; }
+     else /* they're equal */
+     {
+       result.push(a[ai]);
+       ai++;
+       bi++;
+     }
+  }
+
+  return result;
+}
+
+var eaterMethods = {
+	'default': function(foods) {
+		return foods.shift()
+	}
+}
+
+// Eater class - holds the state and specific strategy of an eater
+// Params:
+//  description - name
+//  alergies - an array of food categories this eater will not eat
+//  favorites - an array of food categories that this eater prefers 
+//  pickFood - a function used to pick the next food. Takes a list of foods. Returns the food hash. (defaults to eaterMethods.default )
+function Eater(description,alergies,favorites,pickFood) {
+	//  denyFood - a function used to pick all foods this won't eat. Returns a list of foods.
+	this.description = description;
+	this.alergies = alergies || [];
+	this.favorites = favorites || [];
+	this.pickFood = pickFood || eaterMethods.default;
+	this.denyFood = function(foods,alergies) {
+		alergies = alergies.sort();
+		//console.log("alergies = "+ alergies);
+		return foods.filter(function (value,index,array) {
+			result = intersect_safe(alergies,value.Categories).length != 0;
+			//console.log("The result = "+ result);
+			return result
+		});
+	};
+	this.eaten = [];
+	this.denied = [];
+	this.toEat = [];
+}
+// setup the internal state for a whole bunch of the foods.
+// Parameters:
+// foods = list of foods.
+Eater.prototype.setupFoodStore = function(foods) {
+	//console.log("this.alergies = "+ this.alergies);
+	this.denied = this.denyFood(foods,this.alergies);
+	//console.log("this.denied = "+ this.denied);
+	var dd = this.denied
+	//console.log("in denied? "+ dd.length);
+	this.toEat = $.extend(foods,[]).filter(function (value,index,array) {
+		//console.log("looking at "+ value.Description);
+		result = $.inArray(value,dd) == -1;
+		//console.log("The result = "+ result);
+		return result;
+	});
+	//console.log("to eat? "+ this.toEat.length);
+	//console.log($.sprintf("Eating %d and not eating %d",this.toEat.length,this.denied.length));
+}
+// eat some food the way we'd do it. specify the general goal or default to 2000.
+// Return an array of the elements eaten, otherwise an empty array
+Eater.prototype.eat = function(calories) {
+	calories = calories || 2000;
+	var calsAte = 0;
+	var ate = [];
+	while (calsAte < calories) {
+		var f = this.pickFood(this.toEat);
+		if (f) {
+			this.eaten.push(f);
+			ate.push(f);
+			calsAte += f['Servings Per Container'] * f.Calories;
+			console.log($.sprintf("Ate up to %6d calories for %s",calsAte,f.Description));
+		}
+		else {
+			break;
+		}
+	}
+	return ate;
+}
+
+// given a list of foods, computer the calories * servings
+function computeCalorieCount(foods) {
+	cals = 0;
+	$.each(foods,function(i,v) { cals += v['Servings Per Container'] * v.Calories; });
+	return cals;
+}
+Eater.prototype.calsAte = function() {
+	return computeCalorieCount(this.eaten);
+}
+Eater.prototype.calsDenied = function() {
+	return computeCalorieCount(this.denied);
+}
+Eater.prototype.calsToEat = function() {
+	return computeCalorieCount(this.toEat);
 }
---
+++ test/vizlathon/spec/unit/spec.js
@@ -0,0 +1,148 @@
+describe 'Util.js - utils'
+	before_each
+        foods = {}
+        foods.ingredients = {}
+        foods.ingredients.row0 = { Description: 'Food with nuts' }
+        foods.ingredients.row1 = { Description: 'Nuts' }
+        foods.ingredients.row2 = { Description: 'Bolts' }
+        foods.ingredientRanks = {}
+        foods.ingredientRanks.row0 = {
+            Description: 'Food with nuts',
+            IngredientCategory: ['Eggs','Nuts']
+        }
+        foods.ingredientRanks.row1 = {
+            Description: 'Nuts',
+            IngredientCategory: 'Eggs'
+        }
+        foods.ingredientRanks.row2 = {
+            Description: 'Nuts',
+            IngredientCategory: ''
+        }
+        foods.ingredientRanks.row3 = {
+            Description: 'Nuts',
+            IngredientCategory: ''
+        }
+    end
+
+    it 'combineIngredientsAndRanks function'
+        result = combineIngredientsAndRanks(foods)
+        result.ingredients.length.should.eql 3
+        result.ingredients.filter(function (v,i,a) { return v.Description == 'Food with nuts' }).length.should.eql 1
+        result.ingredients.filter(function (v,i,a) { return v.Description == 'Food with nuts' })[0].Categories.should.eql ['Eggs','Nuts']
+        result.ingredients.filter(function (v,i,a) { return v.Description == 'Nuts' })[0].Ranks.length.should.eql 3
+        result.ingredients.filter(function (v,i,a) { return v.Description == 'Nuts' })[0].Categories.should.eql ['Eggs']
+        result.ingredients.filter(function (v,i,a) { return v.Description == 'Bolts' })[0].Ranks.length.should.eql 0
+    end
+end
+
+describe 'Util.js - Eater'
+	before_each
+		//appendTo = $(fixture('appendable.html'))
+        foods = [{
+            Description: 'Food with nuts',
+            'Servings Per Container': 3,
+            Calories: 200,
+            Ranks: [{
+                    Description: 'Food with nuts',
+                    IngredientCategory: ['Eggs','Nuts']
+                }],
+            Categories: ['Eggs','Nuts']
+        }, {
+            Description: 'Licorice',
+            'Servings Per Container': 3,
+            Calories: 400,
+            Ranks: [{
+                Description: 'Licorice',
+                IngredientCategory: ['Salt','Spice','Sugar','Coloring']
+                }],
+            Categories: ['Salt','Spice','Sugar','Coloring']
+        }]
+	end
+
+    it 'a blank make a constructor'
+        eater = new Eater('Test Eater',[],[])
+        nofoods = []
+        eater.setupFoodStore(nofoods)
+        eater.eaten.length.should.eql 0
+        eater.toEat.length.should.eql 0
+        eater.denied.length.should.eql 0
+    end
+
+    it 'a basic meal'
+        eater = new Eater('Test Eater',[],[])
+        eater.setupFoodStore(foods)
+        eater.eaten.length.should.eql 0
+        eater.denied.length.should.eql 0
+        eater.toEat.length.should.eql 2
+        meal = eater.eat()
+        meal.length.should.eql 2
+        meal.filter(function (v) { return v.Description == 'Food with nuts' }).length.should.eql 1
+        eater.eaten.length.should.eql 2
+        eater.toEat.length.should.eql 0
+        meal = eater.eat()
+        meal.length.should.eql 0
+        eater.calsAte().should.eql 1800
+        eater.calsDenied().should.eql 0
+        eater.calsToEat().should.eql 0
+    end
+
+    it 'no nuts please'
+        eater = new Eater('Test Eater',['Nuts'],[])
+        eater.setupFoodStore(foods)
+        eater.eaten.length.should.eql 0
+        eater.toEat.length.should.eql 1
+        eater.denied.length.should.eql 1
+        meal = eater.eat()
+        meal.length.should.eql 1
+        meal[0].should.have_property 'Description','Licorice'
+        eater.eaten.length.should.eql 1
+        eater.toEat.length.should.eql 0
+        meal = eater.eat()
+        meal.length.should.eql 0
+        eater.calsAte().should.eql 1200
+        eater.calsDenied().should.eql 600
+        eater.calsToEat().should.eql 0
+    end
+end
+
+/*
+describe 'GUI Things: '
+	before_each
+		appendTo = $(fixture('appendable'))
+		len = calendars.length;
+	end
+
+  describe 'the makeList function'
+		it 'show all calendars when none are excluded'
+			makeList(appendTo,'named');
+			appendTo.should.have_tag 'ul'
+			for (var i=0; i<len; i++) {
+				appendTo.find('option[value='+ calendars[i] +']').should.have_tag "<option>"
+			}
+    end
+
+		it 'show all but excluded calendars'
+			makeList(appendTo,'named','julian');
+			appendTo.should.have_tag 'ul'
+			for (var i=0; i<len; i++) {
+				if (calendars[i] != 'julian') {
+					appendTo.find('option[value='+ calendars[i] +']').should.have_tag "<option>"
+				}
+				else {
+					appendTo.find('option[value='+ calendars[i] +']').length.should.eql 0
+				}
+			}
+		end
+  end
+end
+
+
+describe 'Dates: '
+	describe 'to human readable types'
+		it 'check hebrew'
+			implementations['hebrew'].toHTML({year:2010, month:10, day:24}).should.eql "<li>5771, Heshvan 16</li>"
+			implementations['hebrew'].toHTML({year:-1238, month:2, day:6}).should.eql "<li>2522, Adar I 16</li>"
+		end
+	end
+end
+*/
